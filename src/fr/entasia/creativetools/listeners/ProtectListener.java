ed3878c374738319280fe188edd9caa7e28641da
itrooz <team.mortelgames@gmail.com>
2020-10-19 19:40:37 +0200

    item satanizer (validator) : fin ?

--- src/fr/entasia/creativetools/listeners/ProtectListener.java
+++ src/fr/entasia/creativetools/listeners/ProtectListener.java
@@ -1,15 +1,13 @@
 package fr.entasia.creativetools.listeners;
 
 import fr.entasia.creativetools.Main;
-import fr.entasia.creativetools.utils.ItemValider;
-import org.bukkit.Material;
+import fr.entasia.creativetools.utils.ItemSanitizer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class ProtectListener implements Listener {
 
@@ -36,8 +34,6 @@ public class ProtectListener implements Listener {
 
 	@EventHandler
 	public void a(InventoryCreativeEvent e) {
-		if(!ItemValider.validateItem(e.getCursor())){
-			e.setCursor(new ItemStack(Material.AIR));
-		}
+		ItemSanitizer.sanitiseItem(e.getCursor());
 	}
 }
---
+++ src/fr/entasia/creativetools/utils/ItemSanitizer.java
@@ -0,0 +1,113 @@
+package fr.entasia.creativetools.utils;
+
+import fr.entasia.apis.nbt.ItemNBT;
+import fr.entasia.apis.nbt.NBTComponent;
+import fr.entasia.apis.nbt.NBTManager;
+import fr.entasia.apis.utils.ReflectionUtils;
+import net.minecraft.server.v1_15_R1.NBTBase;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftMetaBanner;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftMetaBlockState;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftMetaBook;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ItemSanitizer {
+
+	public static Field unhandledTags;
+	public static Method applyToItem;
+
+	public static Class<?> CraftMetaEnchantedBook;
+	public static Field bannerPatterns;
+
+	static {
+		try {
+			Class<?> CraftMetaItem = ReflectionUtils.getOBCClass("inventory.CraftMetaItem");
+			unhandledTags = CraftMetaItem.getDeclaredField("unhandledTags");
+			unhandledTags.setAccessible(true);
+			applyToItem = CraftMetaItem.getDeclaredMethod("applyToItem", NBTManager.TagCompoundClass);
+			applyToItem.setAccessible(true);
+
+			CraftMetaEnchantedBook = ReflectionUtils.getOBCClass("inventory.CraftMetaItem");
+
+			Class<?> CraftMetaBanner = org.bukkit.craftbukkit.v1_15_R1.inventory.CraftMetaBanner.class;
+			bannerPatterns = CraftMetaBanner.getDeclaredField("patterns");
+			bannerPatterns.setAccessible(true);
+
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public static void sanitiseItem(ItemStack item){
+		try{
+			if(item==null)return;
+			ItemMeta meta = item.getItemMeta();
+			if (meta == null)return;
+			Map<String, NBTBase> map = (Map<String, NBTBase>) unhandledTags.get(meta);
+			map.clear();
+
+			for(Map.Entry<Enchantment, Integer> e : meta.getEnchants().entrySet()) {
+				meta.addEnchant(e.getKey(), e.getValue(), false);
+			}
+			meta.setAttributeModifiers(null);
+
+			if(!sanitiseMeta(meta)){
+				meta = Bukkit.getItemFactory().getItemMeta(Material.STONE);
+			}
+
+
+			NBTComponent nbt = new NBTComponent();
+			applyToItem.invoke(meta, nbt.getRawNBT()); // ca applie bien la method child, et pas celle de CraftMetaItem....
+			ItemNBT.setNBT(item, nbt);
+		}catch(ReflectiveOperationException e){
+			if(item==null)return;
+			item.setType(Material.AIR);
+			item.setItemMeta(null);
+		}
+	}
+
+
+	public static boolean sanitiseMeta(ItemMeta meta){
+		if (meta instanceof CraftMetaBanner) {
+			CraftMetaBanner cmeta = (CraftMetaBanner) meta;
+			if (cmeta.numberOfPatterns() > 32) {
+				cmeta.setPatterns(new ArrayList<>());
+			}
+		} else if (meta instanceof CraftMetaBook) {
+			CraftMetaBook cmeta = (CraftMetaBook) meta;
+			if (cmeta.getPageCount() > 100) {
+				cmeta.pages.subList(0, 100);
+			}
+		} else if (meta instanceof EnchantmentStorageMeta) {
+			EnchantmentStorageMeta cmeta = (EnchantmentStorageMeta)meta;
+			for(Map.Entry<Enchantment, Integer> e : cmeta.getStoredEnchants().entrySet()) {
+				cmeta.addStoredEnchant(e.getKey(), e.getValue(), false);
+			}
+		} else if (meta instanceof SkullMeta) {
+			// bon de base
+		} else if (meta instanceof MapMeta) {
+			// bon de base
+		} else if (meta instanceof PotionMeta) {
+			PotionMeta cmeta = (PotionMeta)meta;
+			cmeta.clearCustomEffects();
+		} else if (meta instanceof FireworkMeta) {
+			FireworkMeta cmeta = (FireworkMeta)meta;
+			if(cmeta.getPower()>3)cmeta.setPower(1);
+			if(cmeta.getEffectsSize()>3)cmeta.clearEffects();
+		} else return false;
+		return true;
+	}
+
+}
